88023009ba80d4b53ebea726796403490d5a4dc5
macelik <a.muhammetcelik@gmail.com>
2023-03-27 15:52:18 +0200

    Update win2.bat

--- win2.bat
+++ win2.bat
@@ -7,7 +7,17 @@ if %errorlevel% neq 0 (
     echo Conda is not installed. Downloading and installing...
     powershell.exe -Command "Invoke-WebRequest https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -OutFile miniconda.exe"
     start /wait miniconda.exe /S /D="%UserProfile%\Miniconda3"
+    del miniconda.exe
+    call "%UserProfile%\Miniconda3\Scripts\activate.bat" "%UserProfile%\Miniconda3"
     set "PATH=%UserProfile%\Miniconda3;%UserProfile%\Miniconda3\Scripts;%UserProfile%\Miniconda3\Library\bin;%PATH%"
+    echo creating the environment
+    call conda init cmd.exe
+    conda activate
+    conda install -c conda-forge mamba
+    mamba env create -f environment.yml
+    conda deactivate
+    CALL conda activate community_tutorial
+    jupyter notebook
 )
 
 rem Check if community_tutorial environment exists
