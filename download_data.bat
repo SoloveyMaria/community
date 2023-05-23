9e8824a8b5244fe1a5ef219bac2890e84f0af9e8
macelik <41537897+macelik@users.noreply.github.com>
2023-05-23 21:08:49 +0200

    Add files via upload

--- download_data.bat
+++ download_data.bat
@@ -1,4 +1,6 @@
 @echo off
+rem Download pre-processed data from zenodo
+echo Downloading Lasry dataset. Download location: docs/showcase_notebooks/Lasry/input_data/...
 powershell -Command "Invoke-WebRequest -Uri https://zenodo.org/record/7962808/files/anno_cells_corr.txt -OutFile docs\showcase_notebooks\Lasry\input_data\anno_cells_corr.txt"
 powershell -Command "Invoke-WebRequest -Uri https://zenodo.org/record/7962808/files/anno_samples_corr.txt -OutFile docs\showcase_notebooks\Lasry\input_data\anno_samples_corr.txt"
 powershell -Command "Invoke-WebRequest -Uri https://zenodo.org/record/7962808/files/counts_corr.csv.gz -OutFile docs\showcase_notebooks\Lasry\input_data\counts_corr.csv.gz"
