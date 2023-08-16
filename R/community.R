39f660e27bea2ab2b88e3f9b3be70b4e7514dedb
macelik <a.muhammetcelik@gmail.com>
2023-08-16 13:38:42 +0200

    edit requirements

--- R/community.R
+++ R/community.R
@@ -2,6 +2,8 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom gridExtra arrangeGrob
+#' @importFrom gridExtra arrangeGrob, grid.arrange
+#' @import ggplot2
+#' @importFrom grid textGrob
 ## usethis namespace: end
 NULL
